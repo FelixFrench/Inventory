2dc3465984900fe8d6492ae5367a5f204b7c8286
FelixFrench <49376258+FelixFrench@users.noreply.github.com>
2026-06-20 15:16:13 +0100

    clear_products.sh also deletes from inventory where quantity=0

--- scripts/clear_products.sh
+++ scripts/clear_products.sh
@@ -7,6 +7,8 @@ sudo systemctl stop fastapi worker listener
 
 sqlite3 "$DB" <<'SQL'
 PRAGMA foreign_keys = ON;
+DELETE FROM inventory
+  WHERE quantity = 0;
 DELETE FROM prices
   WHERE barcode NOT IN (SELECT barcode FROM inventory);
 DELETE FROM product_variants
